e377cb94cb2b34a845441c58d606c5d553136d36
rqr1989 <dontopenthepyxis@gmail.com>
2022-04-13 21:42:51 -0400

    Set up UI panels

---
+++ LookingUpward/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //refernce to player characters RigidBody2d component
+    private Rigidbody2D body;
+    // 
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
